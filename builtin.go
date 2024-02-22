b236f94c61b74bf40cd8f946be9536e4b62f08f5
xiaosongfu <xiaosong.fu@outlook.com>
2024-02-22 21:36:10 +0800

    setup sepolia contracts address

--- builtin.go
+++ builtin.go
@@ -6,12 +6,18 @@ const indexerHost = "https://test-spp-indexer.seedao.tech"
 // Safe Server Host
 const safeHost = "https://test-sns-api.seedao.tech"
 
-// RPCs
-const rpc = "https://eth-goerli.g.alchemy.com/v2/MATWeLJN1bEGTjSmtyLedn0i34o1ISLD"
+//// RPCs
+//const rpc = "https://eth-goerli.g.alchemy.com/v2/MATWeLJN1bEGTjSmtyLedn0i34o1ISLD"
+//// Deployed Addresses
+//const baseRegistrarAddr = "0x4C53Ff1A6a47E7089e1E727f83e7b7aEFCC9796B"             // BaseRegistrar contract address
+//const seedaoRegistrarControllerAddr = "0x78d29889bCa13A477b25933923732FF562DcC7Cd" // SeeDAORegistrarController contract address
+//const publicResolverAddr = "0x6A80eA63cFfc6B10B764e1f26348832835520646"            // PublicResolver contract address
+//const seedaoMinterAddr = "0x618702B04DC94ACca487aE888Fd986adfC2f66d2"              // SeeDAOMinter contract address
+//const seeDAOActivityMinterAddr = "0x41B6ECd5BC6Bca0Ca6F9C1c01FfEa6a6b24fF59f"      // SeeDAOActivityMinter contract address
 
-// Deployed Addresses
-const baseRegistrarAddr = "0x4C53Ff1A6a47E7089e1E727f83e7b7aEFCC9796B"             // BaseRegistrar contract address
-const seedaoRegistrarControllerAddr = "0x78d29889bCa13A477b25933923732FF562DcC7Cd" // SeeDAORegistrarController contract address
-const publicResolverAddr = "0x6A80eA63cFfc6B10B764e1f26348832835520646"            // PublicResolver contract address
-const seedaoMinterAddr = "0x618702B04DC94ACca487aE888Fd986adfC2f66d2"              // SeeDAOMinter contract address
-const seeDAOActivityMinterAddr = "0x41B6ECd5BC6Bca0Ca6F9C1c01FfEa6a6b24fF59f"      // SeeDAOActivityMinter contract address
+// ---> sepolia
+const rpc = "https://eth-sepolia.g.alchemy.com/v2/H43zK7UnIN2v7u2ZoTbizIPnXkylKIZl"
+const baseRegistrarAddr = "0xCaF5eC467ecce6f1a43C361848483a88FEb0d5b4"             // BaseRegistrar contract address
+const seedaoRegistrarControllerAddr = "0x492b9AF316C81FA1D9aA8F975170E0A7Fcd48092" // SeeDAORegistrarController contract address
+const publicResolverAddr = "0x46dbAD41324a616938A0Dac717Eb26aFBC1512EC"            // PublicResolver contract address
+const seedaoMinterAddr = "0xb1a69eb4b8121734Ed6402EEF4aC74a98b212B6F"              // SeeDAOMinter contract address
